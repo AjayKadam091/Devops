5c28060643c341cde424efc24f2f66f27bf65f6e
Ajay K <kadamajay091@gmail.com>
2025-10-13 14:07:40 +0530

    adding s3 bucket details

--- Terraform/practice/Day2/main.tf
+++ Terraform/practice/Day2/main.tf
@@ -12,7 +12,6 @@ resource "aws_instance" "ec2" {
 resource "aws_s3_bucket" "s3_bucket" {
     bucket = var.bucket
     region = var.region
-    acl = "private"
     tags = {
       "name" = "app-bucket-231"
       "Environment" = "Dev"
