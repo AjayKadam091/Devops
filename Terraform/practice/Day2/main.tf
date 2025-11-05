2059c6f25ee25ffa3ad8ae7a4acea12081b40376
Ajay K <kadamajay091@gmail.com>
2025-11-05 14:18:03 +0530

    changed in main.tf

--- Terraform/practice/Day2/main.tf
+++ Terraform/practice/Day2/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "ec2" {
     ami = var.amiID
     instance_type = var.instance_type
-    
+    subnet_id = aws_subnet.public.id
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
